47f67ed638a331882e188ff510315854ea067423
Josh Ethridge <joshua.ethridge13@gmail.com>
2018-11-27 21:24:00 -0500

    Routing basics finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsMenuComponent } from './settings-menu/settings-menu.component';
+import { AboutMenuComponent } from './about-menu/about-menu.component';
+import { StartMenuComponent } from './start-menu/start-menu.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'start', component: StartMenuComponent },
+  { path: 'settings', component: SettingsMenuComponent },
+  { path: 'about', component: AboutMenuComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsMenuComponent,
+    AboutMenuComponent,
+    StartMenuComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
